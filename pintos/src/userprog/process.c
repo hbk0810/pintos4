bfd3612465fc5efcece852e66c828491b76121e1
hbk0810 <hbk08101@gist.ac.kr>
2022-05-29 04:41:11 -0700

    10 fails

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -469,54 +469,6 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 	  upage += PGSIZE;
   }
   return true;
-  /*file_seek (file, ofs);
-  while (read_bytes > 0 || zero_bytes > 0) 
-    {*/
-      /* Calculate how to fill this page.
-         We will read PAGE_READ_BYTES bytes from FILE
-         and zero the final PAGE_ZERO_BYTES bytes. *//*
-      size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
-      size_t page_zero_bytes = PGSIZE - page_read_bytes;
-
-	  struct vm_entry* vme = (struct vm_entry*)malloc(sizeof(struct vm_entry));
-	  if(vme == NULL)
-		  return false;
-	  memset(vme, 0, sizeof(struct vm_entry));
-	  vme->type = VM_BIN;
-	  vme->file = file;
-	  vme->offset = ofs;
-	  vme->read_bytes = page_read_bytes;
-	  vme->zero_bytes = page_zero_bytes;
-	  vme->writable = writable;
-	  vme->vaddr = upage;
-	  insert_vme(&thread_current()->vm, vme);
-	  ofs += page_read_bytes;*/
-      /* Get a page of memory. 
-      uint8_t *kpage = palloc_get_page (PAL_USER);
-      if (kpage == NULL)
-        return false;*/
-
-      /* Load this page. 
-      if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
-        {
-          palloc_free_page (kpage);
-          return false; 
-        }
-      memset (kpage + page_read_bytes, 0, page_zero_bytes);*/
-
-      /* Add the page to the process's address space. 
-      if (!install_page (upage, kpage, writable)) 
-        {
-          palloc_free_page (kpage);
-          return false; 
-        }*/
-
-      /* Advance. *//*
-      read_bytes -= page_read_bytes;
-      zero_bytes -= page_zero_bytes;
-      upage += PGSIZE;
-    }
-  return true;*/
 }
 
 /* Create a minimal stack by mapping a zeroed page at the top of
